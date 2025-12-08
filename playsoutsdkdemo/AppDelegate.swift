ba9cbde69536ee6d4c7733b3197024d3433fefb7
AI&Games <54703084+lkbtboy@users.noreply.github.com>
2025-12-08 15:11:31 +0800

    update sdk key
    update sdk key

--- playsoutsdkdemo/AppDelegate.swift
+++ playsoutsdkdemo/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     private func initEngine() {
         //flutterEngine.
-        flutterEngine.run(withEntrypoint: "main", initialRoute: "/home?channel=playsout&sdkkey=eyJ2ZXIiOiJ2MSIsImNoYW5uZWwiOiJwbGF5c291dCIsInBhY2thZ2VuYW1lIjoiIiwiZXhwIjoxNzYwOTY3ODIwfS5zaWc"); //update if sdkkey expire,look log
+        flutterEngine.run(withEntrypoint: "main", initialRoute: "/home?channel=playsout&sdkkey=eyJ2ZXIiOiJ2MSIsImNoYW5uZWwiOiJwbGF5c291dCIsInBhY2thZ2VuYW1lIjoiIiwiZXhwIjoxNzY3NzY5ODA0fQ==3L"); //update if sdkkey expire,look log
         GeneratedPluginRegistrant.register(with: flutterEngine);
     }
 
